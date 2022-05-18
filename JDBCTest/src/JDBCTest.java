487b9646c8b59c83a81f546e5594360a3c14d7db
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-05-18 18:25:14 +0530

    JDBC basic connection setup

---
+++ JDBCTest/src/JDBCTest.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class JDBCTest {
+	
+	public static void main(String args[]) {
+				
+		try {
+			
+			System.out.println("Connecting to the database..");
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "C##strutsdeveloper", "strutsdeveloper");
+						
+			System.out.println("Connection established!!!");
+			
+			Statement st = con.createStatement();
+			
+			ResultSet rs = st.executeQuery("SELECT * FROM student");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("ID") + " ----> " + rs.getString("NAME"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+}
